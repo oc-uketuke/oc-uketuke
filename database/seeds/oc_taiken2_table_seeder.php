f4893f3504da12a741407f369a5018dc258856eb
s-Zenya <b5037@oic.jp>
2016-09-18 00:49:45 +0900

    seeder

--- database/seeds/oc_taiken2_table_seeder.php
+++ database/seeds/oc_taiken2_table_seeder.php
@@ -13,14 +13,13 @@ class oc_taiken2_table_seeder extends Seeder
     {
       $faker=Faker\Factory::create('ja_JP');
       $taiken1=array(1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19);
-      $taiken2=array(3,4,6,7,12,13,16,18,20);
-      $taiken3=array(2,4,5,10,11,14,15,17,19,20);
-      $taiken4=array(2,3,6,7,10,13,16,18,20,20);
-      $taiken5=array(1,3,4,7,11,12,13,14,15,18,20);
+      $taiken2=array(2,4,5,10,11,14,15,17,19,20);
+      $taiken3=array(2,3,6,7,10,13,16,18,20,20);
+      $taiken4=array(1,3,4,7,11,12,13,14,15,18,20);
       for($i=1;$i<50;$i++){
         $id_rand=rand(1,6);
         $taiken_rand=rand(1,20);
-        $hoge=array(1=>$taiken1,2=>$taiken1,3=>$taiken1,4=>$taiken3,5=>$taiken4,6=>$taiken5);
+        $hoge=array(1=>$taiken1,2=>$taiken1,3=>$taiken1,4=>$taiken2,5=>$taiken3,6=>$taiken4);
         for($j=1;$j<count($hoge[$id_rand]);$j++){
           // if($taiken_rand==$hoge[$id_rand[$j]]){
           //   break;
