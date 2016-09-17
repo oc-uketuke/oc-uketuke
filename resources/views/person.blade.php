02c66ac91111c5277162c6df34b91f56deec6abf
44x1carbon <b4079@oic.jp>
2016-09-18 03:00:18 +0900

    [add]id,クラス名追加

--- resources/views/person.blade.php
+++ resources/views/person.blade.php
@@ -1,28 +1,33 @@
-@extends('layout');
+@extends('layout')
+
+@section('css')
+<link rel="stylesheet" href="{{ asset('assets/css/person.css') }}">
+@endsection
 
 @section('content')
 	<div id="person-info">
-		<div>
-			<div>
+		<div id="person-info-top">
+			<div id="name">
 				<div>
-					<div>フリガナ</div>
+					<div class="name-top">フリガナ</div>
 					<div>名前</div>
 				</div>
 				<div>
-					<div>&nbsp;</div>
+					<div class="name-top">&nbsp;</div>
 					<div>:</div>
 				</div>
 				<div>
-					<div>{{ $person->furigana }}</div>
+					<div class="name-top">{{ $person->furigana }}</div>
 					<div>{{ $person->name }}</div>
 				</div>
 			</div>
-			<div>学年：</div>
+			<div id="school-year">学年：</div>
 		</div>
-		<div>学校名：{{ $person->school_name }}</div>
+
+		<div id="person-info-bottom">学校名：{{ $person->school_name }}</div>
 	</div>
 	<hr>
 	<div id="taiken-list">
-		
+
 	</div>
 @endsection
