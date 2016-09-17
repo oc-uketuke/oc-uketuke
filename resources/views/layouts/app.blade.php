2a932bffb55aa1351047d997c050ca1623fdd42f
fuchi15 <sakuya815@gmail.com>
2016-09-17 22:23:30 +0900

    first commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,11 @@
+<!-- Stored in resources/views/layouts/app.blade.php -->
+<html>
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+    </body>
+</html>
---
+++ resources/views/people-list.blade.php
@@ -0,0 +1,7 @@
+@extends('layout.app');
+
+@section('content')
+<p>
+  hello
+</p>
+@endsection
